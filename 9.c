2f27b7ac21f1cb0ec9fa732b50898b35be10efd6
Vaibhav Tiwari <107205805+Vaibhav882@users.noreply.github.com>
2022-11-01 23:05:09 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    printf("CUBE OF FIRST TEN NATURAL NUMBER  are: ");
+    while(i<=10)
+    {
+        printf("%d ",i*i*i);
+        i++;
+    }
+return 0;
+
+} 
